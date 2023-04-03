3183a136d4aac5935f57d777589c568f0da82419
Mikhail Kozlov <kozlov_ma@icloud.com>
2023-04-04 00:37:43 +0500

    Added moves

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 cd ~/repos/jack_chess/.tetris2nand/nand2tetris/tools/
-./JackCompiler.sh ~/repos/jack_chess/src &
-cd ~/repos/jack_chess/src
-cd ..
-cd bin
-rip *
-cd src
+./JackCompiler.sh ~/repos/jack_chess/src
+cd ~/repos/jack_chess/
+cd bin 
+touch privet 
+rip *  
+cd .. 
+cd src 
 mv *.vm ../bin/
